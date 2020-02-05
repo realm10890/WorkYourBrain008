fc42c6fc582828d6248220f88daf2696573ab3b2
realm10890 <59128537+realm10890@users.noreply.github.com>
2020-02-04 22:24:26 -0800

    Initial Commit

---
+++ WorkYourBrain008/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WorkYourBrain008
+//
+//  Created by Cesar Almendarez on 2/4/20.
+//  Copyright © 2020 Cesar Almendarez. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
